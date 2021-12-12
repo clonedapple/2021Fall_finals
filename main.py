8b56187156a130e34c0208e34c7131af74a9cae1
ta9ay <art)fDABB!NG123>
2021-12-11 20:21:32 -0600

    Adding plots folder. Updated comments in notebook.

--- main.py
+++ main.py
@@ -92,6 +92,92 @@ def read_music_data_files():
 
     return music_df, charts_df, profanity_list
 
+        def popularity_lookup(row):
+            year = row[0]
+            pop_value = popularity[row[1]][year] * 100
+
+def music_processing(charts_df,music_df):
+    charts_df['song'] = charts_df['song'].str.lower()
+    charts_df = charts_df.drop_duplicates(subset=['song', 'artist'])
+    charts_df['artist'] = charts_df['artist'].str.lower()
+
+    merged_music = music_df.merge(charts_df, left_on=['track_name', 'artist_name'], right_on=['song', 'artist'],
+                                  how='inner')
+    post59 = merged_music[merged_music['release_date'] >= 1959][
+        ['artist_name', 'track_name', 'release_date', 'genre', 'lyrics', 'violence', 'obscene', 'topic', 'rank']]
+
+    # low numbers for hip-hop. replacing with pop
+    post59['genre'].replace('hip hop', 'pop', inplace=True)
+
+    return post59
+
+def calculate_prof_percent(row, profanity_list):
+    lyrics = row[4].split(' ')
+    total_length = len(lyrics)
+    counter = 0
+    for word in lyrics:
+        if word in profanity_list:
+            counter += 1
+    return (counter / total_length) * 100
+
+        # normalizing data due to difference in scale
+        cols_to_normalize = ['percent_share', 'percent_prof']
+        temp[cols_to_normalize] = temp[cols_to_normalize].apply(lambda x: (x - x.min()) / (x.max() - x.min()))
+
+def plot_genres(data, popularity):
+    sns.set(rc={'figure.figsize': (11.7, 8.27)})
+    sns.set_theme(style="whitegrid")
+    #     plt.style.use("bmh")
+
+    plt.style.use("seaborn-dark")
+
+    for param in ['figure.facecolor', 'axes.facecolor', 'savefig.facecolor']:
+        plt.rcParams[param] = '#2F3235'  # bluish dark grey
+
+    for param in ['text.color', 'axes.labelcolor', 'xtick.color', 'ytick.color']:
+        plt.rcParams[param] = '0.9'  # very light grey
+
+    plt.rcParams['axes.linewidth'] = 2
+    #     ax.grid(color='#2A3459')  # bluish dark grey, but slightly lighter
+
+    genres = list(data['genre'].unique())
+    for genre in genres:
+        temp = data[data['genre'] == genre]
+
+        def popularity_lookup(row):
+            year = row[0]
+            pop_value = popularity[row[1]][year] * 100
+
+            return pop_value
+
+        temp['percent_share'] = temp.apply(lambda x: popularity_lookup(x), axis=1)
+        temp = temp.drop('genre', 1)
+
+        # normalizing data due to difference in scale
+        cols_to_normalize = ['percent_share', 'percent_prof']
+        temp[cols_to_normalize] = temp[cols_to_normalize].apply(lambda x: (x - x.min()) / (x.max() - x.min()))
+
+        corr = pearsonr(temp['percent_prof'], temp['percent_share'])
+        title_text = f"Genre: {genre.capitalize()}, Correlation value: {corr[0]:.4f}, P-Value: {corr[1]:.4f}"
+
+        sns.lmplot(data=pd.melt(temp, ['release_date']), x='release_date', y='value', hue='variable', height=10,
+                   palette=['#08F7FE', '#FE53BB'], aspect=1.2, scatter_kws={"s": 20}, line_kws={"linewidth": 4}).set(
+            title=title_text)
+
+###############
+
+def read_music_data_files():
+    music_df = pd.read_csv('data//tcc_ceds_music.csv')
+    charts_df = pd.read_csv('data//charts.csv')
+
+    # import profanity words
+    profanity_list = []
+    with open('data//bad_words.txt', 'r') as fp:
+        for row in fp:
+            profanity_list.append(row.replace('\n', ''))
+
+    return music_df, charts_df, profanity_list
+
 
 def music_processing(charts_df,music_df):
     charts_df['song'] = charts_df['song'].str.lower()
@@ -137,26 +223,5 @@ def plot_genres(data, popularity):
     genres = list(data['genre'].unique())
     for genre in genres:
         temp = data[data['genre'] == genre]
-
-        def popularity_lookup(row):
-            year = row[0]
-            pop_value = popularity[row[1]][year] * 100
-
-            return pop_value
-
-        temp['percent_share'] = temp.apply(lambda x: popularity_lookup(x), axis=1)
-        temp = temp.drop('genre', 1)
-
-        # normalizing data due to difference in scale
-        cols_to_normalize = ['percent_share', 'percent_prof']
-        temp[cols_to_normalize] = temp[cols_to_normalize].apply(lambda x: (x - x.min()) / (x.max() - x.min()))
-
-        corr = pearsonr(temp['percent_prof'], temp['percent_share'])
-        title_text = f"Genre: {genre.capitalize()}, Correlation value: {corr[0]:.4f}, P-Value: {corr[1]:.4f}"
-
-        sns.lmplot(data=pd.melt(temp, ['release_date']), x='release_date', y='value', hue='variable', height=10,
-                   palette=['#08F7FE', '#FE53BB'], aspect=1.2, scatter_kws={"s": 20}, line_kws={"linewidth": 4}).set(
-            title=title_text)
-
 
 
