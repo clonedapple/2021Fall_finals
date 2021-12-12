a549260fabc16768d893f10cdac55b6a62c006f8
clonedapple <pvnbrle@gmail.com>
2021-12-11 19:48:32 -0600

    Added hypothesis 2 functions to the main.py file. Added the necessary code for visualizations Viz.ipynb.

--- main.py
+++ main.py
@@ -8,8 +8,9 @@ from scipy.stats import pearsonr
 
 pd.options.mode.chained_assignment = None
 
+###############Hypothesis 1###################
 
-def read_data_files(): # called
+def read_movies_data_files(): # called
     df_1 = pd.read_csv('data//IMDBdata_MainData.csv')
     df_2 = pd.read_csv('data//IMDBdata_MainData2.csv')
     data = pd.concat([df_1, df_2]).drop_duplicates(subset=['Title']).reset_index(drop=True)
@@ -21,7 +22,7 @@ def read_data_files(): # called
 
     return data, kim_data
 
-def processing(mpa_data, kim_data): #called
+def movies_processing(mpa_data, kim_data): #called
     temp_join = pd.merge(mpa_data, kim_data, on='Title')
     temp_join = temp_join[['Title', 'Rated', 'Released', 'Genre', 'Plot', 'Rating']]
 
@@ -77,36 +78,85 @@ def calculate_score_correlation(data):
 
     return correlations
 
+###############Hypothesis 2###################
+
+def read_music_data_files():
+    music_df = pd.read_csv('data//tcc_ceds_music.csv')
+    charts_df = pd.read_csv('data//charts.csv')
+
+    # import profanity words
+    profanity_list = []
+    with open('data//bad_words.txt', 'r') as fp:
+        for row in fp:
+            profanity_list.append(row.replace('\n', ''))
+
+    return music_df, charts_df, profanity_list
 
 
-################## notebook
+def music_processing(charts_df,music_df):
+    charts_df['song'] = charts_df['song'].str.lower()
+    charts_df = charts_df.drop_duplicates(subset=['song', 'artist'])
+    charts_df['artist'] = charts_df['artist'].str.lower()
 
-# mpa_data, kim_data = read_data_files()
-# post92 = processing(mpa_data, kim_data)
-# percentages = calculate_unweighted_percentage(post92)
-#
-# # Normalizing
-# mean_percentage = dict(percentages.mean())
-# normalize_dict = percentages.to_dict()
-#
-# # Normalizing agg score (col=10)
-# post92['weighted_agg'] = post92.apply(calculate_weighted_score,args=[10, mean_percentage, normalize_dict], axis=1)
-# percentages['weighted_kim_score'] = post92.groupby('Year')['weighted_agg'].mean()
-#
-# # Category wise
-# cols_to_process = ['Sex','Violence','Language']
-# for col in cols_to_process:
-#     post92[col] = post92[col].astype('int32')
-#
-# post92['weighted_sex'] = post92.apply(calculate_weighted_score,args=[7, mean_percentage, normalize_dict], axis=1)
-# post92['weighted_violence'] = post92.apply(calculate_weighted_score,args=[8, mean_percentage, normalize_dict], axis=1)
-# post92['weighted_language'] = post92.apply(calculate_weighted_score,args=[9, mean_percentage, normalize_dict], axis=1)
-#
-# percentages['weighted_sex_score'] = post92.groupby('Year')['weighted_sex'].mean()
-# percentages['weighted_violence_score'] = post92.groupby('Year')['weighted_violence'].mean()
-# percentages['weighted_language_score'] = post92.groupby('Year')['weighted_language'].mean()
+    merged_music = music_df.merge(charts_df, left_on=['track_name', 'artist_name'], right_on=['song', 'artist'],
+                                  how='inner')
+    post59 = merged_music[merged_music['release_date'] >= 1959][
+        ['artist_name', 'track_name', 'release_date', 'genre', 'lyrics', 'violence', 'obscene', 'topic', 'rank']]
+
+    # low numbers for hip-hop. replacing with pop
+    post59['genre'].replace('hip hop', 'pop', inplace=True)
+
+    return post59
+
+def calculate_prof_percent(row, profanity_list):
+    lyrics = row[4].split(' ')
+    total_length = len(lyrics)
+    counter = 0
+    for word in lyrics:
+        if word in profanity_list:
+            counter += 1
+    return (counter / total_length) * 100
 
 
-#
-# # Correlation
-# calculate_score_correlation(temp_viz_df)
+def plot_genres(data, popularity):
+    sns.set(rc={'figure.figsize': (11.7, 8.27)})
+    sns.set_theme(style="whitegrid")
+    #     plt.style.use("bmh")
+
+    plt.style.use("seaborn-dark")
+
+    for param in ['figure.facecolor', 'axes.facecolor', 'savefig.facecolor']:
+        plt.rcParams[param] = '#2F3235'  # bluish dark grey
+
+    for param in ['text.color', 'axes.labelcolor', 'xtick.color', 'ytick.color']:
+        plt.rcParams[param] = '0.9'  # very light grey
+
+    plt.rcParams['axes.linewidth'] = 2
+    #     ax.grid(color='#2A3459')  # bluish dark grey, but slightly lighter
+
+    genres = list(data['genre'].unique())
+    for genre in genres:
+        temp = data[data['genre'] == genre]
+
+        def popularity_lookup(row):
+            year = row[0]
+            pop_value = popularity[row[1]][year] * 100
+
+            return pop_value
+
+        temp['percent_share'] = temp.apply(lambda x: popularity_lookup(x), axis=1)
+        temp = temp.drop('genre', 1)
+
+        # normalizing data due to difference in scale
+        cols_to_normalize = ['percent_share', 'percent_prof']
+        temp[cols_to_normalize] = temp[cols_to_normalize].apply(lambda x: (x - x.min()) / (x.max() - x.min()))
+
+        corr = pearsonr(temp['percent_prof'], temp['percent_share'])
+        title_text = f"Genre: {genre.capitalize()}, Correlation value: {corr[0]:.4f}, P-Value: {corr[1]:.4f}"
+
+        sns.lmplot(data=pd.melt(temp, ['release_date']), x='release_date', y='value', hue='variable', height=10,
+                   palette=['#08F7FE', '#FE53BB'], aspect=1.2, scatter_kws={"s": 20}, line_kws={"linewidth": 4}).set(
+            title=title_text)
+
+
+
